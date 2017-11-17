780c185b8706a3b5a2b89ed99231e3ae7e9dd125
petez <pete.zsolt@codeyard.eu>
2017-11-17 21:12:20 +0100

    Initial Commit

---
+++ CYExtensions/CYExtensions.h
@@ -0,0 +1,19 @@
+//
+//  CYExtensions.h
+//  CYExtensions
+//
+//  Created by Zsolt Pete on 2017. 11. 17..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CYExtensions.
+FOUNDATION_EXPORT double CYExtensionsVersionNumber;
+
+//! Project version string for CYExtensions.
+FOUNDATION_EXPORT const unsigned char CYExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CYExtensions/PublicHeader.h>
+
+
