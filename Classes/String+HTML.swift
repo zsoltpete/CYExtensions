e024a7f865343f5c058ab78ed78c028a81e3efa6
petez <pete.zsolt@codeyard.eu>
2017-11-18 22:28:51 +0100

    📈 Changed to public

--- Classes/String+HTML.swift
+++ Classes/String+HTML.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 extension String {
-    var html2AttributedString: NSAttributedString? {
+    
+    public var html2AttributedString: NSAttributedString? {
         do {
             return try NSAttributedString(data: Data(utf8),
                                           options: [.documentType: NSAttributedString.DocumentType.html,
@@ -20,7 +21,8 @@ extension String {
             return nil
         }
     }
-    var html2String: String {
+    
+    public var html2String: String {
         return html2AttributedString?.string ?? ""
     }
 }
